c2b79bd2cfee031ecf65b4d9503f429931a34deb
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-01-15 15:44:21 +0100

    relocated files to src folder

--- src/script/loadJson.js
+++ src/script/loadJson.js
@@ -4,13 +4,13 @@ import Movie from "./movies/renderMovies.js";
 
 export default class loadJsonData {
     async loadCurrentMovies() {
-        const res = await fetch("./script.src/jsonData/currentMovies.json")
+        const res = await fetch("./src/script/jsonData/currentMovies.json")
         const data = await res.json();
         return data.map(mv => new Movie(mv)); 
     }
 
     async loadUpcomingMovies() {
-        const res = await fetch("./script.src/jsonData/upcomingMovies.json")
+        const res = await fetch("./src/script/jsonData/upcomingMovies.json")
         const data = await res.json();
         return data.map(mv => new Movie(mv)); 
     }
