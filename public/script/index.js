9e00c746c1dc18ed09ff665d0281df297181d948
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-01-26 16:27:27 +0100

    rendered filtered screenings in main.hbs

--- public/script/index.js
+++ public/script/index.js
@@ -1,7 +1,20 @@
-import loadScreenings from "./loadScreening.js";
-
 async function loadData() {
-    const screeningData = await loadScreenings();
+    const res = await fetch("http://localhost:5080/api/screeningtime");
+    const data = await res.json(); 
+    
+    data.screen.forEach(showtime => {
+        const li = document.createElement("li");
+        const movieShowTime = document.createElement("span");
+        movieShowTime.innerText = showtime.time; 
+        const movieTitle = document.createElement("a");
+        movieTitle.innerText = showtime.title; 
+        movieTitle.href = `/movies/${showtime.id}`;
+
+        li.append(movieShowTime);
+        li.append(movieTitle); 
+        
+        document.querySelector("#screening-list").append(li); 
+    })
 }; 
 
 loadData();
--- public/script/loadScreening.js
+++ public/script/loadScreening.js
@@ -1,8 +1,42 @@
-const API_BASE = 'https://lernia-kino-cms.herokuapp.com/api';
+import { loadScreenings } from "./apiLoader.js";
 
-export default async function loadScreenings() {
-  const res = await fetch(API_BASE + '/screenings?populate=movie');
-  const payload = await res.json();
-  console.log(payload);
-  return payload.data;
+export class Screenings {
+  constructor (data) {
+    this.title = data.attributes.movie.data.attributes.title; 
+    this.id = data.attributes.movie.data.id; 
+    this.room = data.attributes.room; 
+    this.time = data.attributes.start_time;
+  }
 }
+export async function getScreenings() {
+  const now = new Date();
+  const now2 = new Date();
+  const result = new Date(now2.setDate(now2.getDate() + 6));
+  console.log(now); 
+  console.log(result); 
+  const screen = (await loadScreenings())
+  .filter(obj => {
+    const screeningTime = new Date(obj.time);
+    return screeningTime > now && screeningTime < result;
+  })
+  .slice(0, 10);
+  
+  return {
+    screen
+  }
+}
+
+/*export async function getScreenings() {
+  const now = new Date();
+  const screen = (await loadScreenings())
+  .filter(obj => {
+    const screeningTime = new Date(obj.time);
+    return screeningTime > now;
+  })
+  .slice(0, 10);
+
+  return {
+    screen
+  }
+}
+*/
